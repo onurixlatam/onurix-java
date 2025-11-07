a3fbabd904195b9cc909fa2c380911937da6aef3
Jhoan Zambrano <alejo.ax1306@gmail.com>
2025-11-07 13:11:30 -0500

    #11

--- GroupsAndContacts/ContactList.java
+++ GroupsAndContacts/ContactList.java
@@ -10,7 +10,7 @@ public class GroupList {
 		// TODO Auto-generated method stub
 		var httpClient = HttpClient.newHttpClient();
 
-		var request = HttpRequest.newBuilder(URI.create("https://www.onurix.com/api/v1/group/list?key=AQUI_SU_KEY&client=AQUI_SU_ID&page=AQUI_NUMERO_DE_PAGINA"))
+		var request = HttpRequest.newBuilder(URI.create("https://www.onurix.com/api/v1/contacts/list?key=AQUI_SU_KEY&client=AQUI_SU_ID&page=AQUI_NUMERO_DE_PAGINA"))
 											.GET()
 											.version(HttpClient.Version.HTTP_2)
 											.build();
